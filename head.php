7ba817ef07e9daf33f5e91faf98353539dcbabab
max <maxpolak11@gmail.com>
2021-10-27 09:17:33 +0200

    Robots meta

--- head.php
+++ head.php
@@ -1,5 +1,7 @@
+	
+	<meta name="robots" content="noindex,follow">
 
-	<meta id="Viewport" name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
 
 	<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
